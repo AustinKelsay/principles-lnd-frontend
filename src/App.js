1e2ffd113f4d1e2683f7f6b86323d7832a370bef
austinkelsay <austinkelsay@yahoo.com>
2021-04-09 17:08:38 -0500

    Initial react app setup with parcel

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React, {useEffect, useState} from "react"
+import axios from "axios"
+
+const App = () => {
+
+    return(
+        <div>
+            <h1>App</h1>
+        </div>
+    )
+}
+
+export default App;
