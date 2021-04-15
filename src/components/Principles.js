23d77dd5ed7460f10bef1f06fecbd5df66d5a133
austinkelsay <austinkelsay@yahoo.com>
2021-04-15 10:48:52 -0500

    Starting to setup axios calls, seems to be issue with deployment

--- src/components/Principles.js
+++ src/components/Principles.js
@@ -1,35 +1,31 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import InfiniteScroll from "react-infinite-scroll-component"
 
 const Principles = () => {
+    const [principles, setPrinciples] = useState([])
+
+    useEffect(() => {
+        axios.get('https://principles-lnd.herokuapp.com/principles')
+        .then((res) => {
+            console.log(res)
+        })
+        .catch
+        ((err) => {
+            console.log(err)
+        })
+    },[])
 
     return (
         <div className="principles">
-            <div>
-                <p>Problem:</p>
-                <p>Diagnosis:</p>
-                <p>Change:</p>
-                <p>User:</p>
+                <div>
+                    <p>Problem:</p>
+                    <p>Diagnosis:</p>
+                    <p>Change:</p>
+                    <p>User:</p>
+                </div>
             </div>
-            <div>
-                <p>Problem:</p>
-                <p>Diagnosis:</p>
-                <p>Change:</p>
-                <p>User:</p>
-            </div>
-            <div>
-                <p>Problem:</p>
-                <p>Diagnosis:</p>
-                <p>Change:</p>
-                <p>User:</p>
-            </div>
-            <div>
-                <p>Problem:</p>
-                <p>Diagnosis:</p>
-                <p>Change:</p>
-                <p>User:</p>
-            </div>
-        </div>
-    )
+            )
 }
 
 export default Principles;
