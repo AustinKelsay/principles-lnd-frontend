91abf9096986f8d59fa92b4833dd459aabfedf16
austinkelsay <austinkelsay@yahoo.com>
2021-04-19 08:15:35 -0500

    Principles are now mapping with unique key, now I need to get the username on the principles

--- src/components/Principles.js
+++ src/components/Principles.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import axios from "axios"
 import InfiniteScroll from "react-infinite-scroll-component"
+import { v4 as uuidv4 } from 'uuid';
 
 const Principles = () => {
     const [principles, setPrinciples] = useState([])
@@ -8,6 +9,7 @@ const Principles = () => {
     useEffect(() => {
         axios.get('https://principles-lnd.herokuapp.com/principles')
         .then((res) => {
+            setPrinciples(res.data)
             console.log(res)
         })
         .catch
@@ -18,14 +20,20 @@ const Principles = () => {
 
     return (
         <div className="principles">
-                <div>
-                    <p>Problem:</p>
-                    <p>Diagnosis:</p>
-                    <p>Change:</p>
-                    <p>User:</p>
-                </div>
-            </div>
-            )
+            {
+                principles.map((principle) => {
+                    return(
+                        <div key={uuidv4()}>
+                            <p>Problem: {principle.problem}</p>
+                            <p>Diagnosis: {principle.diagnosis}</p>
+                            <p>Change: {principle.change}</p>
+                            <p>User:</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+        )
 }
 
 export default Principles;
