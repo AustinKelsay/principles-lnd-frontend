ccde0b17d2cea508d914146d723b0a49e7266f65
austinkelsay <austinkelsay@yahoo.com>
2021-07-02 13:06:31 -0600

    Trying to get TS t work with Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from "react-redux";
+import { Reducer } from "./store/Reducer";
+import thunk from "redux-thunk";
 import './index.scss';
 import App from './App';
 
+const store = createStore(Reducer, applyMiddleware(thunk));
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
--- src/store/Reducer.tsx
+++ src/store/Reducer.tsx
@@ -1,15 +1,21 @@
 import {GET_USER, ADD_USER} from './Actions'
 
+type UserState = {
+    user: object;
+    }
+
 const initialState = {
-    user: {}
+    user: {
+
+    }
 }
 
-export function Reducer (state = initialState, action:any) {
+export function Reducer (state:UserState = initialState, action:any) {
     switch(action.type) {
         case GET_USER: {
             return state.user
         }
-        case ADD_USER {
+        case ADD_USER: {
             return {...state, user: action.apyload}
         }
         default: return state;
