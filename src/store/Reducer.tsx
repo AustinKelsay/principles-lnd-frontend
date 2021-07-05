f5d8caa267bca73091d1ab960b6111bbbfe1b9b8
austinkelsay <austinkelsay@yahoo.com>
2021-07-05 17:12:12 -0600

    I tried to do TS right but everytime I assign state to a type in my Reducer I get this strange error I can't debug

--- src/store/Reducer.tsx
+++ src/store/Reducer.tsx
@@ -1,21 +1,36 @@
 import {GET_USER, ADD_USER} from './Actions'
 
 type User = {
-    id: number
+    admin: number | null,
+    cert: string,
+    host: string,
+    id: number,
+    macaroon: string,
+    password: string,
+    pubkey: string,
+    username: string,
 }
 
+
 const initialState:User = {
-    id: 0
+    admin: 0,
+    cert: '',
+    host: '',
+    id: 0,
+    macaroon: '',
+    password: '',
+    pubkey: '',
+    username: '',
 }
 
 export function Reducer (state:any = initialState, action:any) {
     switch(action.type) {
         case GET_USER: {
-            return state.user
+            return state
         }
         case ADD_USER: {
             console.log(action.payload)
-            return {...state, user: action.payload}
+            return {state: action.payload}
         }
         default: return state;
     }
