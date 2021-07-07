e1e313290e2d0d3f8b760ca00e7fd4613c15caf7
austinkelsay <austinkelsay@yahoo.com>
2021-07-06 18:02:16 -0600

    Need to make sure my types in defaultRootState are in order

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,25 @@ import "./components.scss"
 const Header = () => {
     const history = useHistory();
     const user = useSelector(state => state)
-
     console.log(user)
+
     return (
+        // Check if user obj is empty
+        user.id == 0
+        ?
         <div className="header">
             <h1>Principles</h1>
             <h4>A Bitcoin Lightning enabled posting board inspired by Ray Dalio's principles format</h4>
             <div>
                 <button onClick={() => history.push('/login')}>Login</button>
                 <button onClick={() => history.push('/register')}>Sign up</button>
+            </div>
+        </div>
+        :
+        <div className="header">
+            <h1>Principles</h1>
+            <h4>A Bitcoin Lightning enabled posting board inspired by Ray Dalio's principles format</h4>
+            <div>
             </div>
         </div>
     )
